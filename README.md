83d1cac56a3940eac53ef1327e967328202faa21
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-02-08 10:56:45 +0800

    push

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 create nuudel app
 
-> created by [create-create-app](https://github.com/uetchy/create-create-app).
-
 ## Use
 
 ```bash
@@ -15,22 +13,12 @@ npx create-nuudel <name>
 > remove this before publishing your package!
 
 - {{name}} => create-nuudel
-- {{upper name}} => CREATE-NUUDEL
-- {{lower name}} => create-nuudel
-- {{capital name}} => CreateNuudel
-- {{capital name space=true}} => Create Nuudel
-- {{camel name}} => createNuudel
-- {{snake name}} => create_nuudel
-- {{kebab name}} => create-nuudel
-- {{space name}} => create nuudel
-- {{uuid}} => 34cdbb2a-38d7-4573-8420-63f67731ba62
-- {{upper (uuid)}} => 8050D62E-4600-437B-AEB7-42ACA355A2AA
-- {{description}} => create nuudel app
-- {{author}} => Damdinsuren Dorjsuren
-- {{email}} => damdinsuren@live.com
-- {{contact}} => Damdinsuren Dorjsuren <damdinsuren@live.com>
-- {{license}} => MIT
-- {{year}} => 2022
-- {{custom}} =>  (undefined until it is defined in `extra` field in `create` function arguments)
 
 See https://github.com/uetchy/create-create-app#template for the further details.
+
+# git submodules
+
+git submodule add https://github.com/c0des1gn/nuudel.git nuudel
+git submodule init
+git submodule update --remote
+rm -rf .git/modules/nuudel
--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,34 @@
 #!/usr/bin/env node
 
-import { create } from 'create-create-app';
-import { resolve } from 'path';
+import { create } from "create-create-app";
+import { resolve } from "path";
+import { stripIndents } from "common-tags";
 
-const templateRoot = resolve(__dirname, '..', 'templates');
-
-const caveat = `
-This is a caveat!
-You can change this in \`src/cli.ts\`.
-`;
+const templateRoot = resolve(__dirname, "..", "templates");
 
 // See https://github.com/uetchy/create-create-app/blob/master/README.md for other options.
 
-create('create-nuudel', {
+create("create-nuudel", {
   templateRoot,
+  promptForTemplate: false,
+  modifyName: (name) => {
+    const regexDashCase = /^[a-z]+(-[a-z]+)*$/;
+
+    if (!regexDashCase.test(name)) {
+      throw new Error("Project name must be dash-cased. For example my-app");
+    }
+
+    if (!name.includes("-")) {
+      console.log(stripIndents`
+              NOTE: Because your project name is not dash-case,
+              "element" has been added as a suffix.
+          `);
+      return `${name}-element`;
+    }
+
+    return name;
+  },
+  /*
   extra: {
     architecture: {
       type: 'list',
@@ -21,7 +36,12 @@ create('create-nuudel', {
       choices: ['macOS', 'Windows', 'Linux'],
       prompt: 'if-no-arg',
     },
-  },
-  after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
-  caveat,
+  }, // */
+  after: ({ name, answers }) => console.log(`Your project: ${name}.`),
+  caveat: ({ name }) => stripIndents`
+      Successfully created your Nuudel project!
+      Now run:
+      cd ${name}
+      yarn dev
+  `,
 });
